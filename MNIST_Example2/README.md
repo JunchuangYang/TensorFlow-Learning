888b1c63af1ae67a91ca3078f7d88127000f1eb8
JunchuangYang <554002970@qq.com>
2018-12-07 11:17:03 +0800

    modify CNN

--- MNIST_Example2/README.md
+++ MNIST_Example2/README.md
@@ -1,6 +1,51 @@
 先放一放，可以理解基础的卷积神经网络，但是放到代码中各种的维度和参数，我搞不清楚为什么要这样设置，得去再充充电。
 
+---
 
+---
+
+---
+
+卷积神经网络的理解可以参考下面的博客，写的都非常的通俗易懂。
+
+![](images/3.png)
+
+![](images/4.png)
+
+![](images/5.png)
+
+**卷积核是如何进行工作的：**
+
+![](images/6.png)![](images/7.png)
+
+
+
+![](images/8.png)
+
+
+
+在这里列出来两个代码中的函数，其它的解释都在代码中做了注释：
+
+**1.tf.nn.conv2d(x,w,strides=[1,1,1,1],padding='SAME')**
+
+- **x**：input tensor of shape [batch,in_heights,in_width,in_channels] (批次大小，图片长和宽，通道数：黑白图片		为1，彩色图片为3)
+- **w**：filter/kernel tensor of shape [filter_height,filter_width,in_channels,out_channels]
+  ​	滤波器/卷积核
+- **strides**:   strides[0]=strides[1]=1
+  ​		strides[1]:代表x方向的步长
+  ​	 	strides[2]:代表y方向的步长
+- **padding:** 只有两种模式 SAME or VALID
+
+**2.tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')**
+
+- 基本参数和上面的一样
+- **ksize[1,x,y,1]** : x,y代表窗口的长和宽，其它默认为1
+
+
+
+![](images/1.png)
+
+![](images/2.png)
 
 参考博客：
 
--- MNIST_Example2/main.py
+++ MNIST_Example2/main.py
@@ -6,6 +6,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 #定义一个函数，用于初始化所有的权值 W
 def weight_variable(shape):
+    # 生成一个截断的正态分布，标准差为0.1
     initial = tf.truncated_normal(shape,stddev=0.1)
     return tf.Variable(initial)
 
@@ -26,43 +27,62 @@ def max_pool_2x2(x):
 def main():
     mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
 
-    x = tf.placeholder(tf.float32,shape=[None,784]) #输入的数据占位符
+    x = tf.placeholder(tf.float32,shape=[None,784]) #输入的数据占位符 28*28的图片转换为一维的向量
     y_ = tf.placeholder(tf.float32,shape=[None,10]) #输入的标签占位符
-    # 第一层卷积
-    w_conv1 = weight_variable([5,5,1,32])
-    b_conv1 = bias_variable([32])
-    x_image = tf.reshape(x,[-1,28,28,1])
+   
+    # 改变x的格式转为4D的向量[batch,in_height,in_width,in_channels]
+    x_image = tf.reshape(x,[-1,28,28,1])   
+    # 初始化第一层卷积的权值和偏置值
+    w_conv1 = weight_variable([5,5,1,32])#5*5的采样窗口，32个卷积核从1个平面抽取特征
+    b_conv1 = bias_variable([32]) # 每一个卷积核一个偏置值
 
+    # 把x_image和权值向量进行卷积，加上偏置值，然后用用户relu激活函数
     h_conv1 = tf.nn.relu(conv2d(x_image,w_conv1) + b_conv1 )
-    h_pool1 = max_pool_2x2(h_conv1)
+    h_pool1 = max_pool_2x2(h_conv1) # 进行max—pooling
 
     # 第二层卷积
+    # 第一层卷积中用了32个卷积核,所以向第二个卷积层输入时输入32个平面
+    # 64个卷积核从32个平面中抽取特征
     w_conv2 = weight_variable([5,5,32,64])
     b_conv2 = bias_variable([64])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1,w_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    # 密集连接层
-    w_fc1 = weight_variable([7*7*64,1024])
-    b_fc1 = bias_variable([1024])
 
+    # 28*28的图片第一次卷积后还是28*28（padding使用的是SAME，大小不会改变），
+    # 第一次池化后变为14*14（2*2的窗口，步长为2）
+    # 第二次卷积后为14*14，第二次池化后变为7*7
+    # 通过上面的操作后得到了64张7*7的平面
+
+
+    # 密集连接层
+    # 初始化第一个全连接层的权值
+    # 上一层有7*7*64个神经元，全连接层有1024个神经元
+    w_fc1 = weight_variable([7*7*64,1024])
+    b_fc1 = bias_variable([1024]) # 1024个偏置值
+
+    # 把池化层2的输出扁平化为一维，-1 表示任意
     h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
+    # 求第一个全连接层的输出
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,w_fc1) + b_fc1)  #第一个全连接层
 
-    # Dropout
+    # Dropout：部分神经元工作，部分神经元不工作，防止过拟合
     keep_prob = tf.placeholder("float")
     h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
 
     # 输出层
+    # 初始化第二个全连接层
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
 
+    # 计算输出，转化为概率得到输出
     y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
     # 训练和评估模型
-    cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv)) #交叉熵
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy) #梯度下降法
+    cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv)) #交叉熵代价函数
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy) #使用AdamOptimizer进行优化
+    # 结果存放在一个bool列表中，argmax返回一维张量中最大值的所在位置
     correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction,'float')) #精确度计算
     with tf.Session() as sess:
